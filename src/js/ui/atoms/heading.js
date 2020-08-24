4c8413e6df6c079b355754fb8aed634959c8c297
Rotem Ben-Itzhak <rotem@elementor.com>
2020-08-24 11:56:52 +0300

    Adding colors scss file, adding to git ignore the src/js folder.

---
+++ src/js/ui/atoms/heading.js
@@ -0,0 +1,33 @@
+import Utils from 'elementor-app/utils/utils.js';
+
+export default function Heading( props ) {
+	const baseClassName = 'eps',
+		allowedTags = [ 'h1', 'h2', 'h3', 'h4', 'h5', 'h6' ],
+		classes = [
+			props.className,
+		],
+		tagName = allowedTags.includes( props.tag ) ? props.tag : 'h1';
+
+	if ( props.variant ) {
+		classes.push( baseClassName + '-' + props.variant );
+	}
+
+	return React.createElement( tagName, {
+		className: Utils.arrayToClassName( classes ),
+	}, props.children );
+}
+
+Heading.propTypes = {
+	className: PropTypes.string,
+	children: PropTypes.oneOfType( [
+		PropTypes.string,
+		PropTypes.object,
+		PropTypes.arrayOf( PropTypes.object ),
+	] ).isRequired,
+	tag: PropTypes.string,
+	variant: PropTypes.string.isRequired,
+};
+
+Heading.defaultProps = {
+	className: '',
+};
