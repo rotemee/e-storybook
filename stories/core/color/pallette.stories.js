6f7993f2d0cbc2335c3f530b25925863dac49d59
Rotem Ben-Itzhak <rotem@elementor.com>
2020-08-27 15:32:42 +0300

    Taking the colors pallette from the document root CSS vars.

--- stories/core/color/pallette.stories.js
+++ stories/core/color/pallette.stories.js
@@ -8,44 +8,37 @@ export default {
 	component: ColorSwatch,
 };
 
-const colors = [
-	{
-		title: 'Success',
-		color: '#39b54a',
-	},
-	{
-		title: 'CTA',
-		color: '#93003F',
-	},
-	{
-		title: 'Info',
-		color: '#58d0f5',
-	},
-	{
-		title: 'Warning',
-		color: '#fcb92c',
-	},
-	{
-		title: 'Danger',
-		color: '#b01b1b',
-	},
-	{
-		title: 'Dark',
-		color: '#000',
-	},
-	{
-		title: 'Light',
-		color: '#fff',
-	},
-	{
-		title: 'Disabled',
-		color: '#c2cbd2',
-	},
-	{
-		title: 'Text Muted',
-		color: '#d5dadf',
-	},
-];
+const getRootCSSVariable = (styleSheets = document.styleSheets) => {
+	var cssVars = [];
+	// loop each stylesheet
+	for(var i = 0; i < styleSheets.length; i++){
+		// loop stylesheet's cssRules
+		try{ // try/catch used because 'hasOwnProperty' doesn't work
+			for( var j = 0; j < styleSheets[i].cssRules.length; j++){
+				try{
+					// loop stylesheet's cssRules' style (property names)
+					for(var k = 0; k < styleSheets[i].cssRules[j].style.length; k++){
+						let name = styleSheets[i].cssRules[j].style[k];
+						// test name for css variable signiture and uniqueness
+						if(name.startsWith('--') && cssVars.indexOf(name) == -1){
+							cssVars.push(name);
+						}
+					}
+				} catch (error) {}
+			}
+		} catch (error) {}
+	}
+	return cssVars;
+};
+
+const dashCaseToPascalCase = ( string ) => ( string.charAt(0).toUpperCase() + string.slice(1) ).replace( /\b-([a-z])/g, (_, char) => ' ' + char.toUpperCase() );
+
+const colors = getRootCSSVariable().map( ( cssVar ) => {
+	return {
+		title: dashCaseToPascalCase( cssVar.replace( '--', '' ) ),
+		color: `var(${ cssVar })`
+	};
+} );
 
 export const Pallette = () => (
 	<CssGrid columns={5} colMinWidth={50} spacing={0}>
