9f86853e1c3f1915637bbe9335c6c2ebb44e8141
Rotem Ben-Itzhak <rotem@elementor.com>
2020-09-02 11:49:49 +0300

    Removing unnecessary files.

--- auto-components/button.stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { default as Component } from 'elementor-app/ui/molecules/button';
-import Knobs from 'e-storybook/view/utils/knobs';
-
-export const Default = () => {
-    const knobs = Knobs.getKnobs( Component );
-
-    return <Component { ...knobs.props }>{ knobs.children }</Component>;
-};
--- auto-components/dialog-actions.stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { default as Component } from 'elementor-app/ui/dialog/dialog-actions';
-import Knobs from 'e-storybook/view/utils/knobs';
-
-export const Default = () => {
-    const knobs = Knobs.getKnobs( Component );
-
-    return <Component { ...knobs.props }>{ knobs.children }</Component>;
-};
